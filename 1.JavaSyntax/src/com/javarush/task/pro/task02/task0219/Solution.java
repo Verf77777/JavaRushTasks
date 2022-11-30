37b2539241897c005fb0bece51bbbdafe4f23cd0
veraaa <verysia700@gmail.com>
2022-11-30 10:20:44 +0300

    add solution JavaSyntax from task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 1;
+        int age = 20;
+        String documents = "Passport";
+        String behavior = "good";
+    }
+}
