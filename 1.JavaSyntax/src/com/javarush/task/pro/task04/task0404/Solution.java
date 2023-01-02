1d62d145aa4f3870a0e1bb38c57806e1258c6301
veraaa <verysia700@gmail.com>
2023-01-02 15:18:26 +0300

    add solution JavaSyntax from task 0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       int j = 0;
+        while (j < 5) {
+            int i = 0;
+            while (i < 10) {
+                System.out.print("Q");
+                i++;
+            }
+            System.out.println();
+            j++;
+        }
+    }
+}
