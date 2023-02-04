1806661a8288b64b4e4e50c5392e4b8e917f3bf9
veraaa <verysia700@gmail.com>
2023-02-04 17:55:28 +0300

    add solution JavaSyntax from task 0425

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -17,7 +17,14 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++){
             for (int j = 0; j < 10; j++) {
-               System.out.print(land);
+               if (i == 9){
+                   System.out.print(bug);
+               } else if (i == 0 && j == 3) {
+                   System.out.print(robot);
+
+               } else {
+                   System.out.print(land);
+               }
             }
             System.out.println();
         }
