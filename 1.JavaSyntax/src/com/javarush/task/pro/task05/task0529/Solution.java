adc94aa27ede7e8f457aaf59204ad0ab5225f87e
veraaa <verysia700@gmail.com>
2023-04-12 09:45:26 +0300

    add solution JavaSyntax from task 0529

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -14,6 +14,7 @@ public class Solution {
     public static int width = 30;
     public static int height = 10;
     public static String[][] field = new String[height][width];
+    public static int[][] bombs = new int[height][width];
 
     public static void main(String[] args) {
         for (int i = 0; i < field.length; i++) {
@@ -21,12 +22,24 @@ public class Solution {
                 field[i][j] = empty;
             }
         }
-        for (int i = 0; i < field.length; i++){
-           int j = (int)(Math.random()*width);
+        for (int i = 0; i < field.length; i++) {
+            int j = (int) (Math.random() * width);
             field[i][j] = robotank;
         }
-        for (int i = 0; i < field.length; i++){
-            for (int j = 0; j < field[i].length; j++){
+        for (int i = 0; i < bombs.length; i++) {
+            for (int count = 0; count < 10; ) {
+                int j = (int) (Math.random() * width);
+                if (bombs[i][j] == 0) {
+                    bombs[i][j] = 1;
+                    count++;
+                }
+
+            }
+        }
+
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
                 System.out.print(field[i][j]);
             }
             System.out.println();
