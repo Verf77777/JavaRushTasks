f3976d2d3bcab6c76fac4c7cbc154075f5e7bb7c
veraaa <verysia700@gmail.com>
2023-04-24 10:20:08 +0300

    add solution JavaSyntax from task 0622 part 2

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
@@ -15,10 +15,24 @@ public class Solution {
     public static String winPhrase = "Амиго одержал победу над Диабло и выбил из его головы загадочный кристалл.";
     public static String loosePhrase = "Диабло победил Амиго.";
     public static int diabloPosition;
+    public static int amigoLives = 9;
+    public static int diabloLives = 9;
 
     public static void main(String[] args) {
         diabloPosition = getRandomNumber(4);
         findDiablo();
+    }
+    public static void amigoLostLife(){
+        amigoLives = amigoLives -1;
+    }
+    public static void diabloLostLife(){
+        diabloLives = diabloLives -3;
+    }
+    public static int amigoAttacks(){
+        return getRandomNumber(3);
+    }
+    public static int diabloDefends(){
+        return getRandomNumber(3);
     }
     public static void findDiablo(){
         System.out.println(getFirstPositionPhrase);
