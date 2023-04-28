a45214aeeb277830cdb28a5c3080a5fc3f3e3158
veraaa <verysia700@gmail.com>
2023-04-28 09:22:07 +0300

    add solution JavaSyntax from task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int x){
+
+    }
+    public static void universalMethod(int x,int y){
+
+    }
+    public static void universalMethod(String string){
+
+    }
+    public static void universalMethod(int x,String string){
+
+    }
+    public static void universalMethod(String string,int x){
+
+    }
+    public static void universalMethod(String string,int x,int y ){
+
+    }
+    public static void universalMethod(int x,int y,String string){
+
+    }
+    public static void universalMethod(int x,int y,int z){
+
+    }
+    public static void universalMethod(int z,String string,int y){
+
+    }
+
+}
