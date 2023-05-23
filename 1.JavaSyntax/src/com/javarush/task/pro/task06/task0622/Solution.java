135a60086abb9003f109a778d1a8a12b53c5a918
veraaa <verysia700@gmail.com>
2023-05-23 13:08:51 +0300

    add solution JavaSyntax from task 0622 part 3

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
@@ -21,33 +21,51 @@ public class Solution {
     public static void main(String[] args) {
         diabloPosition = getRandomNumber(4);
         findDiablo();
+        battle();
     }
-    public static void amigoLostLife(){
-        amigoLives = amigoLives -1;
+
+    public static void battle() {
+        while (amigoLives > 0 && diabloLives > 0) {
+            if (amigoAttacks() == diabloDefends()) {
+                amigoLostLife();
+                System.out.println(diabloDefendPhrase);
+            } else {
+                diabloLostLife();
+                System.out.println(amigoAttackPhrase);
+            }
+        }
     }
-    public static void diabloLostLife(){
-        diabloLives = diabloLives -3;
+
+    public static void amigoLostLife() {
+        amigoLives = amigoLives - 1;
     }
-    public static int amigoAttacks(){
+
+    public static void diabloLostLife() {
+        diabloLives = diabloLives - 3;
+    }
+
+    public static int amigoAttacks() {
         return getRandomNumber(3);
     }
-    public static int diabloDefends(){
+
+    public static int diabloDefends() {
         return getRandomNumber(3);
     }
-    public static void findDiablo(){
+
+    public static void findDiablo() {
         System.out.println(getFirstPositionPhrase);
         Scanner scanner = new Scanner(System.in);
-        while (true){
+        while (true) {
             int number = scanner.nextInt();
-            if (number == diabloPosition){
+            if (number == diabloPosition) {
                 System.out.println(findDiabloPhrase);
                 break;
-            }
-            else {
+            } else {
                 System.out.println(getPositionPhrase);
             }
         }
     }
+
     public static int getRandomNumber(int range) {
         return (int) (Math.random() * range) + 1;
     }
